063adcf0b1ba4fc628e64ebc2f4a927cd5b0c9ce
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-08-12 00:11:10 -0300

    Regra de negocio

--- app/src/main/java/com/example/appfitness/ImcActivity.kt
+++ app/src/main/java/com/example/appfitness/ImcActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
+import java.lang.Math.pow
 
 class ImcActivity : AppCompatActivity() {
 
@@ -26,7 +28,11 @@ class ImcActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            Log.i("Values", "${editWeight.text.toString()} - ${editHeight.text.toString()}")
+            val imcResult = calculateImc(editWeight.text.toString(), editHeight.text.toString())
+            Log.i("Values", "${String.format("%.1f", imcResult)}")
+
+            val imcResponseId = imcResponse(imcResult)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
 
         }
 
@@ -39,6 +45,24 @@ class ImcActivity : AppCompatActivity() {
                 editHeight.text.toString().isNotEmpty() &&
                 !editHeight.text.toString().startsWith("0")
         )
+    }
+
+    private fun calculateImc(weight: String, height: String): Double {
+        return weight.toDouble() / pow(height.toDouble() / 100.0, 2.0)
+    }
+
+    @StringRes
+    private fun imcResponse(imc: Double): Int {
+        when (imc) {
+            in 0.0..15.0 -> return R.string.imc_severely_low_weight
+            in 15.1..16.0 -> return R.string.imc_very_low_weight
+            in 16.1..18.5 -> return R.string.imc_low_weight
+            in 18.6..25.0 -> return R.string.normal
+            in 25.1..30.0 -> return R.string.imc_high_weight
+            in 30.1..35.0 -> return R.string.imc_so_high_weight
+            in 35.1..40.0 -> return R.string.imc_severely_high_weight
+            else -> return R.string.imc_extreme_weight
+        }
     }
 
 }
