ad097960fcca5b6f451a481070c21bc4958efe66
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-08-11 23:05:17 -0300

    Validacao form

--- app/src/main/java/com/example/appfitness/ImcActivity.kt
+++ app/src/main/java/com/example/appfitness/ImcActivity.kt
@@ -2,10 +2,43 @@ package com.example.appfitness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_heigth)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if (!validate()){
+                Toast.makeText(this, R.string.fields_message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            Log.i("Values", "${editWeight.text.toString()} - ${editHeight.text.toString()}")
+
+        }
+
     }
+
+    private fun validate(): Boolean {
+        return (
+                editWeight.text.toString().isNotEmpty() &&
+                !editWeight.text.toString().startsWith("0") &&
+                editHeight.text.toString().isNotEmpty() &&
+                !editHeight.text.toString().startsWith("0")
+        )
+    }
+
 }
