cf0a296c65ffda169377913eea42b26c610fc7ad
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-08-15 23:04:10 -0300

    Hide keyboard

--- app/src/main/java/com/example/appfitness/ImcActivity.kt
+++ app/src/main/java/com/example/appfitness/ImcActivity.kt
@@ -1,9 +1,11 @@
 package com.example.appfitness
 
+import android.content.Context
 import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -45,6 +47,8 @@ class ImcActivity : AppCompatActivity() {
                 show()
             }
 
+            val service = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            service.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
 
         }
 
