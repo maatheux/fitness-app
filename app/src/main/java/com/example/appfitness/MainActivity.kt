b6965d8387720a2c2dda1318cdbded35b1961c2a
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-08-16 23:49:22 -0300

    RecyclerView and Adapter

--- app/src/main/java/com/example/appfitness/MainActivity.kt
+++ app/src/main/java/com/example/appfitness/MainActivity.kt
@@ -1,24 +1,53 @@
 package com.example.appfitness
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnImc: LinearLayout
+//    private lateinit var btnImc: LinearLayout
+    private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
+        val adapter = MainAdapter()
+        rvMain = findViewById(R.id.rv_main)
+        rvMain.adapter = adapter
+        rvMain.layoutManager = LinearLayoutManager(this)
 
-        btnImc.setOnClickListener {
-            val i = Intent(this, ImcActivity::class.java)
-            startActivity(i)
+//        btnImc = findViewById(R.id.btn_imc)
+//
+//        btnImc.setOnClickListener {
+//            val i = Intent(this, ImcActivity::class.java)
+//            startActivity(i)
+//        }
+
+    }
+
+    private inner class MainAdapter : RecyclerView.Adapter<MainViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+            return MainViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            Log.i("position", "$position")
+        }
+
+        override fun getItemCount(): Int {
+            return 30
         }
 
     }
+
+    private class MainViewHolder(view: View) : RecyclerView.ViewHolder(view) { }
+    // e a celula, ou o layout, em si
+
 }
