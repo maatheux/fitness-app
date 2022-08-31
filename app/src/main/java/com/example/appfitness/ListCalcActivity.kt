6b3b1b22fb7340b0109488abdb22b2ff1d93b557
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-08-30 23:55:49 -0300

    Criando lista de resultados

--- app/src/main/java/com/example/appfitness/ListCalcActivity.kt
+++ app/src/main/java/com/example/appfitness/ListCalcActivity.kt
@@ -1,10 +1,18 @@
 package com.example.appfitness
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.appfitness.model.Calc
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
 
 class ListCalcActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,10 +27,48 @@ class ListCalcActivity : AppCompatActivity() {
             val response = dao.getRegisterByType(type)
 
             runOnUiThread {
-                Log.i("Teste", "Response: $response")
+                val adapter = ListCalAdapter(response)
+                val rvListCalc = findViewById<RecyclerView>(R.id.rvListCalc)
+                rvListCalc.adapter = adapter
+                rvListCalc.layoutManager = LinearLayoutManager(this)
             }
 
         }.start()
 
     }
+
+    private inner class ListCalAdapter(private val calcItems : List<Calc>) : RecyclerView.Adapter<ListCalAdapter.ListCalcViewHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListCalcViewHolder {
+            val view = layoutInflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+            return ListCalcViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ListCalcViewHolder, position: Int) {
+            val currentItem = calcItems[position]
+            if (currentItem.type == "imc") holder.bind(currentItem)
+        }
+
+        override fun getItemCount(): Int {
+            return calcItems.size
+        }
+
+        private inner class ListCalcViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun bind(item: Calc) {
+                val textItem = itemView.findViewById<TextView>(android.R.id.text1)
+
+                val createdDate = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    item.createdDate.toString().format(DateTimeFormatter.ofPattern("dd/mm/yyyy"))
+                } else {
+                    item.createdDate.toLocaleString()
+                }
+
+                textItem.text = "${item.id} - ${String.format("%.2f", item.res)} - $createdDate"
+
+            }
+        }
+
+
+    }
+
 }
