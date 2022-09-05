6dc0fb513c3dd4ead2a710723fcb6f3b04ee0731
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-09-05 20:59:45 -0300

    Empilhamento de activities

--- app/src/main/java/com/example/appfitness/ImcActivity.kt
+++ app/src/main/java/com/example/appfitness/ImcActivity.kt
@@ -77,6 +77,7 @@ class ImcActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.menu_search) {
+            finish()
             openListActivity()
         }
 
