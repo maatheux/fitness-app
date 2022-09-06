eed1a0c1957d9838dae4839db3cfeb4cbc61d8b6
Matheus Lima <matheusmarinho.lima@hotmail.com>
2022-09-05 22:17:40 -0300

    Layout e AutoCompleteText

--- app/src/main/java/com/example/appfitness/MainActivity.kt
+++ app/src/main/java/com/example/appfitness/MainActivity.kt
@@ -46,7 +46,8 @@ class MainActivity : AppCompatActivity() {
                         startActivity(intent)
                     }
                     2 -> {
-                        //
+                        var intent = Intent(this@MainActivity, TmbActivity::class.java)
+                        startActivity(intent)
                     }
                 }
         }
---
+++ app/src/main/java/com/example/appfitness/TmbActivity.kt
@@ -0,0 +1,22 @@
+package com.example.appfitness
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+
+class TmbActivity : AppCompatActivity() {
+
+    private lateinit var lifestyle: AutoCompleteTextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tmb)
+
+        lifestyle = findViewById(R.id.auto_lifestyle)
+        val items = resources.getStringArray(R.array.tmb_lifestyle)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, items)
+        lifestyle.setAdapter(adapter)
+
+    }
+}
